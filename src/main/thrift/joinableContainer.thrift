d9d2b33127097f65d08acf7e23ce5937eaa4db09
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-03-27 16:07:50 +0200

    Add stream manipulation methods in joinable container
    Not used a different file to avoid struct include problems

--- src/main/thrift/joinableContainer.thrift
+++ src/main/thrift/joinableContainer.thrift
@@ -6,6 +6,29 @@ struct JoinableContainer {
 	1: required joinable.Joinable parent;
 }
 
+exception JoinableContainerNotFoundException {
+}
+
+struct JoinableStream {
+	1: required joinable.Joinable paren;
+}
+
+exception JoinableStreamNotFoundException {
+}
+
+enum StreamType {
+	AUDIO,
+	VIDEO,
+}
+
 service JoinableContainerService extends joinable.JoinableService {
-	// TODO: Add methods here
+	JoinableStream getJoinableStream(1: JoinableContainer container, 2: StreamType value) throws (1: JoinableContainerNotFoundException jcnfs, 2: JoinableStreamNotFoundException jsnf);
+
+	list<JoinableStream> getJoinableStreams(1: JoinableContainer container) throws (1: JoinableContainerNotFoundException jcnfs);
+}
+
+service JoinableStreamService extends joinable.JoinableService {
+	JoinableContainer getContainer(1: JoinableStream stream) throws (1: JoinableStreamNotFoundException jsnf);
+
+	StreamType getType(1: JoinableStream stream) throws (1: JoinableStreamNotFoundException jsnf);
 }
