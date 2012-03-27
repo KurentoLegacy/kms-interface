73acb084be1c71348b07466e3e7043c1ed986264
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-03-27 11:12:09 +0200

    Add methods to JoinableService

--- src/main/thrift/joinable.thrift
+++ src/main/thrift/joinable.thrift
@@ -2,10 +2,25 @@ namespace * com.kurento.kms.api
 
 include "mediaObject.thrift"
 
+enum Direction {
+	SEND,
+	RECV,
+	SEND_RECV,
+}
+
 struct Joinable {
 	1: required mediaObject.MediaObject parent;
 }
 
+exception JoinableNotFoundException {
+	1: required string description,
+}
+
+exception JoinException {
+	1: required string description,
+}
+
 service JoinableService extends mediaObject.MediaObjectService {
-	// TODO: Add methods here
+	void join(1: Joinable from, 2: Joinable to, 3: Direction direction) throws (1: JoinableNotFoundException jnfe, 2: JoinException je),
+	void unjoin(1: Joinable from, 2: Joinable to) throws (1: JoinableNotFoundException jnfe, 2: JoinException je)
 }
