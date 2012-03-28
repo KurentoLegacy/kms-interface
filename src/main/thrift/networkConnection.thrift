8ab976b13b180f888374f63a92dfcca2325b5268
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-03-28 15:00:04 +0200

    Add negotiation methods to NetworkConnectionService

--- src/main/thrift/networkConnection.thrift
+++ src/main/thrift/networkConnection.thrift
@@ -1,6 +1,7 @@
 namespace * com.kurento.kms.api
 
 include "joinableContainer.thrift"
+include "sessionSpec.thrift"
 
 enum NetworkConnectionConfig {
 	RTP,
@@ -15,6 +16,15 @@ struct NetworkConnection {
 exception NetworkConnectionNotFoundException {
 }
 
+exception NegotiationException {
+	1: required string description,
+}
+
 service NetworkConnectionService extends joinableContainer.JoinableContainerService {
-	
+	sessionSpec.SessionSpec generateOffer(1: NetworkConnection nc) throws (1: NetworkConnectionNotFoundException ncnfe),
+	sessionSpec.SessionSpec processAnswer(1: NetworkConnection nc, 2: sessionSpec.SessionSpec anwser) throws (1: NetworkConnectionNotFoundException ncnfe, 2: NegotiationException ne),
+	sessionSpec.SessionSpec processOffer(1: NetworkConnection nc, 2: sessionSpec.SessionSpec offer) throws (1: NetworkConnectionNotFoundException ncnfe, 2: NegotiationException ne),
+
+	sessionSpec.SessionSpec getLocalDescriptor(1: NetworkConnection nc) throws (1: NetworkConnectionNotFoundException ncnfe),
+	sessionSpec.SessionSpec getRemoteDescriptor(1: NetworkConnection nc) throws (1: NetworkConnectionNotFoundException ncnfe, 2: NegotiationException ne),
 }
