f6fd85794b3230df15c22b1213d45e4dca7bbf98
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-04 13:04:29 +0200

    change name of parameters from mediaSessionId to mediaSession

--- src/main/thrift/mediaSession.thrift
+++ src/main/thrift/mediaSession.thrift
@@ -13,11 +13,11 @@ exception MediaSessionNotFoundException {
 }
 
 service MediaSessionService extends mediaObject.MediaObjectService {
-	networkConnection.NetworkConnection createNetworkConnection(1: MediaSession mediaSessionId, 2: list<networkConnection.NetworkConnectionConfig> config) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
-	void deleteNetworkConnection(1: MediaSession mediaSessionId, 2: networkConnection.NetworkConnection networConnection) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe, 3: networkConnection.NetworkConnectionNotFoundException ncnfe);
-	list<networkConnection.NetworkConnection> getNetworkConnections(1: MediaSession mediaSessionId) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
+	networkConnection.NetworkConnection createNetworkConnection(1: MediaSession mediaSession, 2: list<networkConnection.NetworkConnectionConfig> config) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
+	void deleteNetworkConnection(1: MediaSession mediaSession, 2: networkConnection.NetworkConnection networConnection) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe, 3: networkConnection.NetworkConnectionNotFoundException ncnfe);
+	list<networkConnection.NetworkConnection> getNetworkConnections(1: MediaSession mediaSession) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
 
-	mixer.Mixer createMixer(1: MediaSession mediaSessionId, 2: list<mixer.MixerConfig> config) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
-	void deleteMixer(1: MediaSession mediaSessionId, 2: mixer.Mixer mixer) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe, 3: mixer.MixerNotFoundException mnfe);
-	list<mixer.Mixer> getMixers(1: MediaSession mediaSessionId) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
+	mixer.Mixer createMixer(1: MediaSession mediaSession, 2: list<mixer.MixerConfig> config) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
+	void deleteMixer(1: MediaSession mediaSession, 2: mixer.Mixer mixer) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe, 3: mixer.MixerNotFoundException mnfe);
+	list<mixer.Mixer> getMixers(1: MediaSession mediaSession) throws (1: common.MediaServerException mse, 2: MediaSessionNotFoundException msnfe);
 }
