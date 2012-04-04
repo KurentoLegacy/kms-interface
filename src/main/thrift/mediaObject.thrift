1d1ef3ec220e9ada9ab666d7d03e94b8b597fa62
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-04-04 09:56:33 +0200

    Change the size of object identifier to 64 bits

--- src/main/thrift/mediaObject.thrift
+++ src/main/thrift/mediaObject.thrift
@@ -7,7 +7,7 @@ namespace * com.kurento.kms.api
  */
 const i32 DEFAULT_TIMEOUT = 60;
 
-typedef i32 ObjectId
+typedef i64 ObjectId
 
 struct MediaObject {
 	1: ObjectId id,
@@ -19,15 +19,15 @@ exception MediaObjectNotFoundException {
 service MediaObjectService {
 	/**
 	 * Notifies the remote party that an object is already in use
-	 * 
+	 *
 	 * @param objectId The object identifier
 	 * @param timeout The time until other timeout is required (in seconds)
 	 */
 	void ping(1: MediaObject mediaObject, 2: i32 timeout = DEFAULT_TIMEOUT) throws (1: MediaObjectNotFoundException monf),
-	
+
 	/**
 	 * Releases the given media object
-	 * 
+	 *
 	 * @param mediaObject The mediaObject that will be released
 	 */
 	void release(1: MediaObject mediaObject) throws (1: MediaObjectNotFoundException monf),
