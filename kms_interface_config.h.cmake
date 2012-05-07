118ff59d715107564758c87a3da7555bf2eb1c47
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2012-05-07 16:30:15 +0200

    Add version kms_interface_config.h

---
+++ kms_interface_config.h.cmake
@@ -0,0 +1,7 @@
+#ifndef __KMS_INTERFACE_CONFIG_H__
+#define __KMS_INTERFACE_CONFIG_H__
+
+/* Version */
+#cmakedefine KMS_INTERFACE_VERSION "@KMS_INTERFACE_VERSION@"
+
+#endif /* __KMS_INTERFACE_CONFIG_H__ */
