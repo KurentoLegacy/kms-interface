0cfb5c57283b1546491058cd43b916012a7f9219
Santiago Gala <sgala@apache.org>
2013-09-26 18:13:40 +0200

    Add Source headers to files
    Change-Id: Ib1851f359b212104e13a21b9af078e1cb08a31a6

--- src/main/thrift/mediaCommandDataTypes.thrift
+++ src/main/thrift/mediaCommandDataTypes.thrift
@@ -1,19 +1,17 @@
 /*
-kms-interface: Thrift interfaces for mediaserver
-Copyright (C) 2013 Tikal Technologies
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3
-as published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * (C) Copyright 2013 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
 
 namespace java com.kurento.kms.thrift.api
 namespace cpp kurento
