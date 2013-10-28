d46e89598fb700bd1267b92aa3d89bb75601bd64
David Fernandez <d.fernandezlop@gmail.com>
2013-10-28 12:22:25 +0100

    Update thrift interface to allow setting windows in pointer detector
    Change-Id: Ib16611b9ca26bd6868e9391a592ff2e9b91885f6

--- src/main/thrift/KmsMediaPointerDetectorFilterType.thrift
+++ src/main/thrift/KmsMediaPointerDetectorFilterType.thrift
@@ -17,13 +17,45 @@ namespace java com.kurento.kms.thrift.api
 namespace cpp kurento
 namespace * Kurento
 
+struct KmsMediaPointerDetectorButton {
+  1: required i32 upRightCornerX,
+  2: required i32 upRightCornerY,
+  3: required i32 width,
+  4: required i32 height,
+  5: required string windowID
+}
+
+typedef list<KmsMediaPointerDetectorButton> KmsMediaPointerDetectorButtonList
 /**
  * PointerDetectorFilter Extends KmsMediaObject
  *
  * Constructors:
  * PointerDetectorFilter (KmsMediaObjectConstructorParams);
+ * PointerDetectorFilter (KmsMediaObjectConstructorParams, KmsMediaPointerDetectorButtonList);
  */
 const string TYPE_NAME = "PointerDetectorFilter";
+const string CONSTRUCTOR_PARAMS_DATA_TYPE = "KmsMediaPointerDetectorButtonList";
+
+/* METHODS */
+/**
+ * This method allows to define a new button window.
+ * void addWindow (KmsMediaPointerDetectorButton buttonProperty);
+ */
+const string ADD_NEW_WINDOW = "addWindow";
+const string ADD_NEW_WINDOW_PARAM_BUTTON_PROPERTY= "buttonProperty";
+
+/**
+ * This method allows to remove a existing button window.
+ * void removeWindow (string windowID);
+ */
+const string REMOVE_WINDOW = "removeWindow";
+const string REMOVE_WINDOW_PARAM_WINDOW_ID = "windowID";
+
+/**
+ * This method allows to clear all existing button windows.
+ * void clearWindows ();
+ */
+const string CLEAR_WINDOW = "clearWindows";
 
 /* EVENTS */
 
