49e0b01690c9cce3b48353b13d1abec690ed3ecd
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2013-11-04 10:55:42 +0100

    HttpEndPoint: Change live parameter by terminateOnEOS
    Change-Id: I230944b136b0dc107c7d572a0d2d25f6de6e49d9

--- src/main/thrift/KmsMediaHttpEndPointType.thrift
+++ src/main/thrift/KmsMediaHttpEndPointType.thrift
@@ -32,7 +32,7 @@ const string CONSTRUCTOR_PARAMS_DATA_TYPE = "KmsMediaHttpEndPointConstructorPara
 
 struct KmsMediaHttpEndPointConstructorParams {
   2: optional i32 disconnectionTimeout
-  3: optional bool live
+  3: optional bool terminateOnEOS
 }
 
 /* METHODS */
