e0fa5af2ca139a1799bd3361140be748749b60d3
Ivan Gracia <izanmail@gmail.com>
2013-11-21 14:37:05 +0100

    Changed names of method params
    Change-Id: Id36c053b5e67910c8faa31b968b4642283e502b8

--- src/main/thrift/KmsMediaServer.thrift
+++ src/main/thrift/KmsMediaServer.thrift
@@ -86,59 +86,59 @@ service KmsMediaServerService {
   ////////////////////////////////////////////////////////////////////////////////
 
   //Invoke this method each GarbagePeriod seconds in order to avoid the MediaObject to be garbage collected
-  void keepAlive(1: KmsMediaObjectRef mediaObjectRef) throws (1: KmsMediaServerException mse);
+  void keepAlive(1: KmsMediaObjectRef mediaObject) throws (1: KmsMediaServerException mse);
   //Explicity release a MediaObject form memory. All its descendants will be also released and collected.
-  void release(1: KmsMediaObjectRef mediaObjectRef) throws (1: KmsMediaServerException mse);
+  void release(1: KmsMediaObjectRef mediaObject) throws (1: KmsMediaServerException mse);
 
   //Invoke this for receiving events from a MediaObject. This method returns a callbackToken that needs to be used for unsubscribing and that will be present on all events.
-  string subscribeEvent(1: KmsMediaObjectRef mediaObjectRef, 2: string eventType, 3: string handlerAddress, 4: i32 handlerPort) throws (1: KmsMediaServerException mse);
+  string subscribeEvent(1: KmsMediaObjectRef mediaObject, 2: string type, 3: string handlerAddress, 4: i32 handlerPort) throws (1: KmsMediaServerException mse);
   //Invoke this for stop receiving events from a MediaObject. The parameter callbackToken must match the one returned by subscribe method.
-  void unsubscribeEvent(1: KmsMediaObjectRef mediaObjectRef, 2: string callbackToken) throws (1: KmsMediaServerException mse);
+  void unsubscribeEvent(1: KmsMediaObjectRef mediaObject, 2: string callbackToken) throws (1: KmsMediaServerException mse);
 
   //Invoke this for receiving error notifications from a MediObject. This method returns a callbackToken that needs to be used for unsubscribing and that will be present on all notifications.
-  string subscribeError(1: KmsMediaObjectRef mediaObjectRef, 2: string handlerAddress, 3: i32 handlerPort) throws (1: KmsMediaServerException mse);
+  string subscribeError(1: KmsMediaObjectRef mediaObject, 2: string handlerAddress, 3: i32 handlerPort) throws (1: KmsMediaServerException mse);
   //Invoke this for stop receiving error notifications from a MediaObject. The parameter callbackToken must match the one returned by errorSubscribe method.
-  void unsubscribeError(1: KmsMediaObjectRef mediaObjectRef, 2: string callbackToken) throws (1: KmsMediaServerException mse);
+  void unsubscribeError(1: KmsMediaObjectRef mediaObject, 2: string callbackToken) throws (1: KmsMediaServerException mse);
 
   //Invoke a method in a mediaObject
-  KmsMediaInvocationReturn invoke(1: KmsMediaObjectRef mediaObjectRef, 2: string methodName, 3: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
+  KmsMediaInvocationReturn invoke(1: KmsMediaObjectRef mediaObject, 2: string method, 3: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
 
   //Returns the parent (object that created it) of a MediaObject
-  KmsMediaObjectRef getParent(1: KmsMediaObjectRef mediaObjectRef) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef getParent(1: KmsMediaObjectRef mediaObject) throws (1: KmsMediaServerException mse);
   //Returns the pipeline to which this MediaObjects belong, or the pipeline itself if the argument references a pipeline
-  KmsMediaObjectRef getMediaPipeline(1: KmsMediaObjectRef mediaObjectRef) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef getMediaPipeline(1: KmsMediaObjectRef mediaObject) throws (1: KmsMediaServerException mse);
 
    /////////////////////////////////////////////////////////////////////////////////
   // Methods associated to MediaPipeline object
   /////////////////////////////////////////////////////////////////////////////////
   KmsMediaObjectRef createMediaPipeline() throws (1: KmsMediaServerException mse);
   KmsMediaObjectRef createMediaPipelineWithParams(1: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
-  KmsMediaObjectRef createMediaElement(1: KmsMediaObjectRef mediaPipeline, 2: string elementType) throws (1: KmsMediaServerException mse);
-  KmsMediaObjectRef createMediaElementWithParams(1: KmsMediaObjectRef mediaPipeline, 2: string elementType, 3: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
-  KmsMediaObjectRef createMediaMixer(1: KmsMediaObjectRef mediaPipeline, 2: string mixerType) throws (1: KmsMediaServerException mse);
-  KmsMediaObjectRef createMediaMixerWithParams(1: KmsMediaObjectRef mediaPipeline, 2: string mixerType, 3: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef createMediaElement(1: KmsMediaObjectRef pipeline, 2: string type) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef createMediaElementWithParams(1: KmsMediaObjectRef pipeline, 2: string type, 3: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef createMediaMixer(1: KmsMediaObjectRef pipeline, 2: string type) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef createMediaMixerWithParams(1: KmsMediaObjectRef pipeline, 2: string type, 3: map<string, KmsMediaParam> params) throws (1: KmsMediaServerException mse);
 
   /////////////////////////////////////////////////////////////////////////////////
   // Methods associated to MediaElement objects
   /////////////////////////////////////////////////////////////////////////////////
-  list<KmsMediaObjectRef> getMediaSrcs(1: KmsMediaObjectRef mediaElement) throws (1: KmsMediaServerException mse);
-  list<KmsMediaObjectRef> getMediaSinks(1: KmsMediaObjectRef mediaElement)  throws (1: KmsMediaServerException mse);
-  list<KmsMediaObjectRef> getMediaSrcsByMediaType(1: KmsMediaObjectRef mediaElement, 2: KmsMediaType mediaType) throws (1: KmsMediaServerException mse);
-  list<KmsMediaObjectRef> getMediaSinksByMediaType(1: KmsMediaObjectRef mediaElement, 2: KmsMediaType mediaType) throws (1: KmsMediaServerException mse);
-  list<KmsMediaObjectRef> getMediaSrcsByFullDescription(1: KmsMediaObjectRef mediaElement, 2: KmsMediaType mediaType, 3: string description) throws (1: KmsMediaServerException mse);
-  list<KmsMediaObjectRef> getMediaSinksByFullDescription(1: KmsMediaObjectRef mediaElement, 2: KmsMediaType mediaType, 3: string description) throws (1: KmsMediaServerException mse);
-  void connectElements(1: KmsMediaObjectRef srcMediaElement, 2: KmsMediaObjectRef sinkMediaElement) throws (1: KmsMediaServerException mse);
-  void connectElementsByMediaType(1: KmsMediaObjectRef srcMediaElement, 2: KmsMediaObjectRef sinkMediaElement, 3: KmsMediaType mediaType) throws (1: KmsMediaServerException mse);
-  void connectElementsByFullDescription(1: KmsMediaObjectRef srcMediaElement, 2: KmsMediaObjectRef sinkMediaElement, 3: KmsMediaType mediaType, 4: string mediaDescription) throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getMediaSrcs(1: KmsMediaObjectRef element) throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getMediaSinks(1: KmsMediaObjectRef element)  throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getMediaSrcsByMediaType(1: KmsMediaObjectRef element, 2: KmsMediaType type) throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getMediaSinksByMediaType(1: KmsMediaObjectRef element, 2: KmsMediaType type) throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getMediaSrcsByFullDescription(1: KmsMediaObjectRef element, 2: KmsMediaType type, 3: string description) throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getMediaSinksByFullDescription(1: KmsMediaObjectRef element, 2: KmsMediaType type, 3: string description) throws (1: KmsMediaServerException mse);
+  void connectElements(1: KmsMediaObjectRef source, 2: KmsMediaObjectRef sink) throws (1: KmsMediaServerException mse);
+  void connectElementsByMediaType(1: KmsMediaObjectRef source, 2: KmsMediaObjectRef sink, 3: KmsMediaType type) throws (1: KmsMediaServerException mse);
+  void connectElementsByFullDescription(1: KmsMediaObjectRef source, 2: KmsMediaObjectRef sink, 3: KmsMediaType type, 4: string mediaDescription) throws (1: KmsMediaServerException mse);
 
   /////////////////////////////////////////////////////////////////////////////////
   // Methods associated to MediaPad objects
   /////////////////////////////////////////////////////////////////////////////////
-  void connect(1: KmsMediaObjectRef mediaSrc, 2: KmsMediaObjectRef mediaSink) throws (1: KmsMediaServerException mse);
-  void disconnect(1: KmsMediaObjectRef mediaSrc, 2: KmsMediaObjectRef mediaSink) throws (1: KmsMediaServerException mse);
-  list<KmsMediaObjectRef> getConnectedSinks(1: KmsMediaObjectRef mediaSrc) throws (1: KmsMediaServerException mse);
-  KmsMediaObjectRef getConnectedSrc(1: KmsMediaObjectRef mediaSinkRef) throws (1: KmsMediaServerException mse);
-  KmsMediaObjectRef getMediaElement(1: KmsMediaObjectRef mediaPadRef) throws (1: KmsMediaServerException mse);
+  void connect(1: KmsMediaObjectRef source, 2: KmsMediaObjectRef sink) throws (1: KmsMediaServerException mse);
+  void disconnect(1: KmsMediaObjectRef source, 2: KmsMediaObjectRef sink) throws (1: KmsMediaServerException mse);
+  list<KmsMediaObjectRef> getConnectedSinks(1: KmsMediaObjectRef source) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef getConnectedSrc(1: KmsMediaObjectRef sink) throws (1: KmsMediaServerException mse);
+  KmsMediaObjectRef getMediaElement(1: KmsMediaObjectRef pad) throws (1: KmsMediaServerException mse);
 
   /////////////////////////////////////////////////////////////////////////////////
   // Methods associated to Mixer objects
