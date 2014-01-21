1ebc59cc76e2f63b9d4d85c84b06426ed055f598
David Fernandez <d.fernandezlop@gmail.com>
2014-01-21 09:14:39 +0100

    Add a generic type to define a image region in the thrift interface
    Change-Id: I5205740213dfdbea077406af136e36ae4fc70e28

---
+++ src/main/thrift/KmsMediaComputerVision.thrift
@@ -0,0 +1,29 @@
+/*
+ * (C) Copyright 2014 Kurento (http://kurento.org/)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+
+namespace java com.kurento.kms.thrift.api
+namespace cpp kurento
+namespace * Kurento
+
+struct KmsMediaImagePoint {
+  1: required i32 x,
+  2: required i32 y
+}
+
+struct KmsMediaImageRegion {
+  1: required KmsMediaImagePoint point,
+  2: required i32 width,
+  3: required i32 height
+}
